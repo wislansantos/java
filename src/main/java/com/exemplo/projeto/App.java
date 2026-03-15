ad15f70f6e32fd097dea745455519b66720eef38
Wislan Santos j <160436045+wislansantos@users.noreply.github.com>
2026-03-15 19:17:54 +0000

    cofigs

--- src/main/java/com/exemplo/projeto/App.java
+++ src/main/java/com/exemplo/projeto/App.java
@@ -1,8 +1,29 @@
 package com.exemplo.projeto;
 
+import java.util.Scanner;
 
 public class App {
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.print("Qual é o seu nome? ");
+        String nome = sc.nextLine();
+        Pessoa p = new Pessoa(nome);
+        System.out.println("Bem vindo " + p.getNome());
+    }
+}
+
+class Pessoa {
+    private String nome;
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 }
